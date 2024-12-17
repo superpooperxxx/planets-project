83f44f013317bde7ebead1ab48d106909914f69b
Kyrylo Haiduk <kyrylo@talentpath.co>
2024-12-17 12:16:04 +0200

    done

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const { parse } = require('csv-parse');
-const fs = require('fs');
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
 const habitablePlanets = [];
 
-function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
 
-fs.createReadStream('kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
+fs.createReadStream(path.resolve(__dirname, "kepler_data.csv"))
+  .pipe(
+    parse({
+      delimiter: ",",
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitable(data)) {
       habitablePlanets.push(data);
     }
   })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
+  .on("error", (error) => console.log(error))
+  .on("end", () => {
+    console.log(habitablePlanets.map((planet) => planet["kepler_name"]));
     console.log(`${habitablePlanets.length} habitable planets found!`);
   });
-
